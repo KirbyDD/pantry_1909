3f0559af2ba1f6b1bff2276761e1711056f6294d
DeMarcus Kirby <tkd024@gmail.com>
2019-11-05 10:57:20 -0700

    Add cookbook class

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -7,11 +7,11 @@ require_relative '../lib/cookbook.rb'
 
 class CookbookTest < Minitest::Test 
   def setup
-    @cheese = Ingredient.new("Cheese", "oz", 100)
+    @cheese = Ingredient.new("Cheese", "C", 100)
     @mac = Ingredient.new("Macaroni", "oz", 30)
     @mac_and_cheese = Recipe.new("Mac and Cheese")
-    @mac_and_cheese.add_ingredient(@cheese, 2)
     @mac_and_cheese.add_ingredient(@mac, 8)
+    @mac_and_cheese.add_ingredient(@cheese, 2)
     @cookbook = Cookbook.new
   end
 
@@ -34,7 +34,11 @@ class CookbookTest < Minitest::Test
     @cookbook.add_recipe(@mac_and_cheese)
     @cookbook.add_recipe(burger)
 
-    expected = [{:name=>"Mac and Cheese", :details=>{:ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}], :total_calories=>440}}, {:name=>"Burger", :details=>{:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"}, {:ingredient=>"Bun", :amount=>"100 g"}], :total_calories=>500}}]
+    expected = [{:name=>"Mac and Cheese", 
+      :details=>{:ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"}, 
+      {:ingredient=>"Cheese", :amount=>"2 C"}], :total_calories=>440}}, 
+      {:name=>"Burger", :details=>{:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"}, 
+      {:ingredient=>"Bun", :amount=>"100 g"}], :total_calories=>500}}]
     assert_equal expected, @cookbook.summary
   end
 
